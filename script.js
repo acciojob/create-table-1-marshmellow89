1c3e75cabe416a1ac64e18424deb741899733af1
marshmellow89 <pritamsingh100app@gmail.com>
2025-02-03 00:30:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function insert_Row() {
-    //Write your code here
+const clcbutton=document.getelementbytype('button');
+const newtd=document.getelementbyid('newtd')
+clcbutton.addeventlistener('click',function insert_Row() {
+	const newrow=document.createelement('tr');
+	newrow.innerhtml="<tr><td>New Cell1</td><td>New Cell1</td>";
+	newtd.appendchild('newrow')
+	
+})
+
+
   
   
 }
